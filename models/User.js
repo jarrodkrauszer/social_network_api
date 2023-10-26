1963f0681088dfc3e74991cac45ce6b583ed0cda
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-10-26 18:53:16 -0400

    added the User model to the User.js file

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/.test(v)
+      }
+    }
+  },
+  thoughts: [{ 
+    type: Schema.Types.ObjectId,
+     ref: 'Thought',
+    }],
+  friends: [{ 
+    type: Schema.Types.ObjectId,
+      ref: 'User',
+    }],
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
