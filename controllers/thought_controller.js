161f6c5ba6c4425e60f73edd6dc2e3af490d7a17
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-10-27 17:33:29 -0400

    started working on thought routes

--- controllers/thought_controller.js
+++ controllers/thought_controller.js
@@ -1,3 +1,28 @@
 const { User, Thought } = require('../models');
 
-module.exports = {};
+module.exports = {
+  async getAll(req, res) {
+    try {
+       const thoughts = await Thought.find();
+
+       res.json(thoughts);
+
+    } catch(err) {
+      console.log(err);
+      res.status(500).json({ message: 'No thoughts found.'});
+    }
+  },
+
+  async getOneById(req, res) {
+    try {
+      const thought = await Thought.findById(req.params.id)
+        .populate('reactions');
+
+    res.json(thought);
+
+    } catch(err) {
+      console.log(err);
+      res.status(500).json({ message: 'No users found.'});
+    }
+  },
+};
